7b0aae8954f16faaff24913297195edeb3831e03
kelvingohcp <kelvingcp89@gmail.com>
2017-05-11 10:34:22 +0800

    update js file

--- js/dev-env.js
+++ js/dev-env.js
@@ -4,6 +4,7 @@ Vue.component('v-dev-env', {
 		<div class="other">
 			<div class="o-bg">
 				<h1 style="margin-top:40px;">{{title}}</h1>
+                                <h2>Some Basic Knowledge ... </h2>
 				<div v-for="(_data, _t) of data">
 					<h3 >{{_t}}</h3>
 					<p v-for="mes of _data"> {{mes}} </p>
@@ -14,13 +15,32 @@ Vue.component('v-dev-env', {
 	props: ['show'],
 	data: function () {
             var menu_name = {'en':this.$root.$data['menu']['4']['e_name'],'cn':this.$root.$data['menu']['4']['name']};
+            //if (menu_name['cn'] != '' || this.$root.$data['menu']['4']['name'].length > 0){
+            if (this.$root.$data['menu']['4']['name'].length > 0){
+                titles = menu_name['cn'] + " ／ " + menu_name['en'];
+            }else{
+                titles = menu_name['en'];
+            }
 		return {
-                    title: menu_name['cn'] + " ／ " + menu_name['en'],
+                    title: titles,
                     bg_src: "background-image: url('img/tiles_white.jpg');background-repeat: repeat;",
                     data: { 
-                        "workstation": [
-                                "LAMP [ Linux + Apache + Mysql + PHP ] with any laptop",
+                        "WHAT I DO": [
+                            "BackEnd developer of a websites build from HTML ,CSS ,Javascript, PHP, mysql",
+                            "if need maybe able do some simple basic web server configuration",
                         ],
+                        "SKILLS": [
+                            "LAMP ,JavaScript ,MVC - (cakephp , laraval)",
+                        ],
+                        "LANGUAGES":[
+                            "Mandarin Chinese, Cantonese, English, Malay",
+                        ],
+                        "My TODO List in 2017 year":[
+                            "Learn some of my intrusting modern language or js",
+                            "meet new friends",
+                            "buy more snack to my pets (2 dogs and 3 cats) ",
+                        ]
+                            
                     }
 		}
 	}
